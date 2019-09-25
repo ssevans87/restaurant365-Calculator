846c5c87638e87df2405caf35a3eded4dcd60c2a
Skyler Evans <skyler.evans@gmail.com>
2019-09-25 15:28:00 -0700

    Completed Step 1
    All requirments of step one completed and tested

--- Calculator365/Calculator.cs
+++ Calculator365/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Calculator365
 {
@@ -6,12 +7,68 @@ namespace Calculator365
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculator calc = new Calculator();
+            if(args.Length >= 1 )Console.WriteLine(calc.Calculate(args[0]));
         }
 
+        //perform calculation on given string
         public string Calculate(string arg1)
         {
-            return "";
+            //list of valid numbers to add together
+            long[] tokens;
+
+            //list of delimiters
+            List<string> delimiters = new List<string>();
+
+            long result = 0;
+
+            //sepperating numbers
+            string[] tempTokens = SeperateNumbers(arg1, delimiters);
+
+            if (tempTokens.Length > 2) return "More than 2 numbers";
+
+            //checking each token if it is a valid number
+            tokens = GetValidArray(tempTokens);
+
+
+            //checking for no valid numbers
+            if (tokens.Length == 0) result = 0;
+            else if (tokens.Length == 1) result = tokens[0];
+            else
+            {
+                foreach (long l in tokens)
+                {
+                    try { result = checked(result + l); }
+                    catch (OverflowException) { return "Overflow"; }
+                }
+            }
+
+            return result.ToString();
+        }
+
+        //seperating numbers by delimiters
+        public string[] SeperateNumbers(string toSeperate, List<string> newDelimiters)
+        {
+
+            string[] tempTokens = toSeperate.Split(',');
+
+
+            return tempTokens;
+        }
+
+        //return only valid numbers
+        public long[] GetValidArray(string[] toCheck)
+        {
+            List<long> result = new List<long>();
+            //checking each token if it is a valid number
+            foreach (string s in toCheck)
+            {
+                long tempLong;
+                if (long.TryParse(s, out tempLong)) result.Add(tempLong);
+            }
+
+
+            return result.ToArray();
         }
     }
 }
--- Calculator365Test/CalculatorTest.cs
+++ Calculator365Test/CalculatorTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Calculator365;
 
@@ -6,6 +8,153 @@ namespace Calculator365Test
     [TestClass]
     public class CalculatorTest
     {
+        
+        [TestMethod]
+        public void testSeperateNumbers1()
+        {
+            //Setup
+            string arg1 = "1";
+            List<string> arg2 = new List<string>();
+            string[] expected = {"1"};
+            string[] actual;
+            Calculator calc = new Calculator();
+
+            //SeperateNumbers(string toSeperate, List<string> newDelimiters)
+            //Assert
+            actual = calc.SeperateNumbers(arg1, arg2);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void testSeperateNumbers2()
+        {
+            //Setup
+            string arg1 = "1,2";
+            List<string> arg2 = new List<string>();
+            string[] expected = { "1","2" };
+            string[] actual;
+            Calculator calc = new Calculator();
+
+            //SeperateNumbers(string toSeperate, List<string> newDelimiters)
+            //Assert
+            actual = calc.SeperateNumbers(arg1, arg2);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void testSeperateNumbers3()
+        {
+            //Setup
+            string arg1 = "1,k";
+            List<string> arg2 = new List<string>();
+            string[] expected = { "1", "k"};
+            string[] actual;
+            Calculator calc = new Calculator();
+
+            //SeperateNumbers(string toSeperate, List<string> newDelimiters)
+            //Assert
+            actual = calc.SeperateNumbers(arg1, arg2);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void testSeperateNumbers4()
+        {
+            //Setup
+            string arg1 = "k,1";
+            List<string> arg2 = new List<string>();
+            string[] expected = {"k","1" };
+            string[] actual;
+            Calculator calc = new Calculator();
+
+            //SeperateNumbers(string toSeperate, List<string> newDelimiters)
+            //Assert
+            actual = calc.SeperateNumbers(arg1, arg2);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void testGetValidArray1()
+        {
+            //Setup
+            string[] arg1 = { "1" };
+            List<string> arg2 = new List<string>();
+            long[] expected = {1 };
+            long[] actual;
+            Calculator calc = new Calculator();
+
+            //GetValidArray(string[] toCheck)
+            //Assert
+            actual = calc.GetValidArray(arg1);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void testGetValidArray2()
+        {
+            //Setup
+            string[] arg1 = { "1", "-2" };
+            List<string> arg2 = new List<string>();
+            long[] expected = { 1,-2 };
+            long[] actual;
+            Calculator calc = new Calculator();
+
+            //GetValidArray(string[] toCheck)
+            //Assert
+            actual = calc.GetValidArray(arg1);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void testGetValidArray3()
+        {
+            //Setup
+            string[] arg1 = { "1", "k" };
+            List<string> arg2 = new List<string>();
+            long[] expected = { 1};
+            long[] actual;
+            Calculator calc = new Calculator();
+
+            //GetValidArray(string[] toCheck)
+            //Assert
+            actual = calc.GetValidArray(arg1);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        //check for overflow
+        [TestMethod]
+        public void OverFlow()
+        {
+            //Setup
+            string arg1 = "9223372036854775807";
+            string arg2 = "1";
+            string expected = "Overflow";
+            string actual;
+            Calculator calc = new Calculator();
+
+
+            //Assert
+            actual = calc.Calculate(arg1 + ',' + arg2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //testing with no input
+        [TestMethod]
+        public void NoArg()
+        {
+            //Setup
+            string arg1 = "";
+            string expected = "0";
+            string actual;
+            Calculator calc = new Calculator();
+
+
+            //Assert
+            actual = calc.Calculate(arg1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //testing with one positive number
         [TestMethod]
         public void OnePos()
         {
@@ -20,5 +169,160 @@ namespace Calculator365Test
             actual = calc.Calculate(arg1);
             Assert.AreEqual(expected, actual);
         }
+
+        //testing with one negative number
+        [TestMethod]
+        public void OneNeg()
+        {
+            //Setup
+            string arg1 = "-1";
+            string expected = "-1";
+            string actual;
+            Calculator calc = new Calculator();
+
+
+            //Assert
+            actual = calc.Calculate(arg1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //testing with one invalid char
+        [TestMethod]
+        public void OneInv()
+        {
+            //Setup
+            string arg1 = "z";
+            string expected = "0";
+            string actual;
+            Calculator calc = new Calculator();
+
+
+            //Assert
+            actual = calc.Calculate(arg1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //testing with one invalid string
+        [TestMethod]
+        public void OneInv2()
+        {
+            //Setup
+            string arg1 = "zzz";
+            string expected = "0";
+            string actual;
+            Calculator calc = new Calculator();
+
+
+            //Assert
+            actual = calc.Calculate(arg1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //testing with two positive numbers
+        [TestMethod]
+        public void TwoPos()
+        {
+            //Setup
+            string arg1 = "1";
+            string arg2 = "2";
+            string expected = "3";
+            string actual;
+            Calculator calc = new Calculator();
+
+
+            //Assert
+            actual = calc.Calculate(arg1 + ',' + arg2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //testing with two negative numbers
+        [TestMethod]
+        public void TwoNeg()
+        {
+            //Setup
+            string arg1 = "-1";
+            string arg2 = "-2";
+            string expected = "-3";
+            string actual;
+            Calculator calc = new Calculator();
+
+
+            //Assert
+            actual = calc.Calculate(arg1 + ',' + arg2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //testing with two opposite sign numbers
+        [TestMethod]
+        public void TwoOpp()
+        {
+            //Setup
+            string arg1 = "1";
+            string arg2 = "-2";
+            string expected = "-1";
+            string actual;
+            Calculator calc = new Calculator();
+
+
+            //Assert
+            actual = calc.Calculate(arg1 + ',' + arg2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //testing with one valid one invalid
+        [TestMethod]
+        public void MixedVal()
+        {
+            //Setup
+            string arg1 = "1";
+            string arg2 = "p";
+            string expected = "1";
+            string actual;
+            Calculator calc = new Calculator();
+
+
+            //Assert
+            actual = calc.Calculate(arg1 + ',' + arg2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //testing with one valid one invalid
+        [TestMethod]
+        public void MixedVal2()
+        {
+            //Setup
+            string arg1 = "p";
+            string arg2 = "1";
+            string expected = "1";
+            string actual;
+            Calculator calc = new Calculator();
+
+
+            //Assert
+            actual = calc.Calculate(arg1 + ',' + arg2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //testing with more than 2 values
+        [TestMethod]
+        public void tooLong()
+        {
+            //Setup
+            string arg1 = "p";
+            string arg2 = "1";
+            string arg3 = "5";
+            string expected = "More than 2 numbers";
+            string actual;
+            Calculator calc = new Calculator();
+
+
+            //Assert
+            actual = calc.Calculate(arg1 + ',' + arg2 + ',' + arg3);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+
     }
 }
